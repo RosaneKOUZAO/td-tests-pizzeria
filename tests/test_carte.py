ef257d8242337ffe520651a8d2ebfbe5f1003081
meva4 <rosanedela85@gmail.com>
2026-02-18 14:09:22 +0100

    Setup test environment

---
+++ tests/test_carte.py
@@ -0,0 +1,21 @@
+import pytest
+from pizzeria.carte import CartePizzeria, CartePizzeriaException
+from unittest.mock import Mock
+def test_carte_est_vide_initialement():
+    pass
+
+
+def test_ajout_pizza():
+    pass
+
+
+def test_nb_pizzas():
+    pass
+
+
+def test_remove_pizza():
+    pass
+
+
+def test_remove_pizza_inexistante():
+    pass
